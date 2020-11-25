de0d98c2ddd81e1df1bc1fdad97e45e0f5742d5b
tgcosta82 <74377653+tgcosta82@users.noreply.github.com>
2020-11-25 13:45:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,87 @@
-<h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<h5>Mudando agora com o pessoal da turma</h5>
-<h4>
-  <? 
-  $i = 6;
-  $i = $i * 350;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gama Academy</title>
 
-  echo " O resultado da variável feita em PHP é: $i";
-  ?>
-</h4>
+  
+  <link rel="stylesheet" href="..\css\biblioteca_estilos.css">
+
+
+
+</head>
+
+<body>
+
+    <table border="0" width="100%" class="menu">
+        <tr>
+            <td align="center"><img src="..\imagens\logo.png"></td>
+            <td align="center">QUEM SOMOS</td>
+            <td align="center">CURSOS ONLINE</td>
+            <td align="center">GAMA EXPERIENCE</td>
+            <td align="center">PARA EMPRESAS</td>
+            <td align="center"><a href="..\html\contatos.html">CONTATO</a></td>
+            <td align="center">CONTEÚDO</td>
+
+        </tr>
+    </table>
+
+    <table border="0" cellpadding="10" cellspacing="5" width="100%">
+        <tr>
+            <td> <img src="..\imagens\imagen1.png"> </td>
+            <td>
+                <p>
+                    As escolas tradicionais trabalham com metodologias que não conseguem suprir
+                    as novas demandas do mundo. Preparam profissionais que não são capazes
+                    de assumir vagas de emprego no mercado atual e, dificilmente, lidar com os avanços tecnológicos.
+                    Não falam sobre soft skills, habilidades altamente requisitadas. E não abordam a importância da
+                    multidisciplinaridade
+                </p>
+                <p>A <b><u>Gama Academy</u></b> forma profissionais com quatro características fundamentais:</p>
+
+                <ul type="square">
+                    <li>1. Vontade de Aprender e Ensinar</li>
+                    <li>2. Soft Skills</li>
+                    <li>3. Mindset Empreendedor</li>
+                    <li>4. Inovação Criativa</li>
+
+                </ul>
+            </td>
+        </tr>
+
+    </table>
+
+    <div align="center">
+        <h3>Vídeo Institucional</h3>
+   
+    <video controls=" autoplay="true" width="400" height="300">
+        <source src="..\videos\gama.mp4" />
+    
+    </video>
+
+
+
+    </div>
+
+    
+
+
+
+
+
+
+
+    <!--<ol type="A">
+        <li>1. Vontade de Aprender e Ensinar</li>
+        <li>2. Soft Skills</li>
+        <li>3. Mindset Empreendedor</li>
+        <li>4. Inovação Criativa</li>
+
+    </ol> -->
+
+
+</body>
+
+</html>
